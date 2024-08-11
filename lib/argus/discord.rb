37ff31319a319a7cfafc0b3072c0c0e33a1631e4
Matt Culpepper <mculp@users.noreply.github.com>
2024-08-10 21:42:21 -0500

    Initial commit

---
+++ lib/argus/discord.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "discord/version"
+
+module Argus
+  module Discord
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/argus/discord_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Argus::Discord do
+  it "has a version number" do
+    expect(Argus::Discord::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
